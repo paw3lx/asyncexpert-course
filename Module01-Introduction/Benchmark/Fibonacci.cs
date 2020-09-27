8d7dc85d7323eab604c075786cd5b05fae89d0eb
paw3lx <paw3lx@gmail.com>
2020-09-27 11:43:37 +0200

    Added Iterative and RecursiveWithMemoization impl

--- Module01-Introduction/Benchmark/Fibonacci.cs
+++ Module01-Introduction/Benchmark/Fibonacci.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 
 namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
@@ -26,14 +27,43 @@ namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
         [ArgumentsSource(nameof(Data))]
         public ulong RecursiveWithMemoization(ulong n)
         {
-            return 0;
+            Dictionary<ulong, ulong> dict = new Dictionary<ulong, ulong>();
+            
+            Func<ulong, Dictionary<ulong, ulong>, ulong> fibCached = null;
+            fibCached = (number, d) =>
+            { 
+                if (number == 1 || number == 2) return 1;
+                
+                if (d.ContainsKey(number))
+                {
+                    return d[number];
+                }
+
+                var result = fibCached(number - 2, d) + fibCached(number - 1, d);
+                d.Add(number, result);
+
+                return result;
+            };
+            
+            return fibCached(n, dict);
         }
         
         [Benchmark]
         [ArgumentsSource(nameof(Data))]
         public ulong Iterative(ulong n)
         {
-            return 0;
+            if(n <= 1) {
+                return n;
+            }
+            ulong a = 1;
+            ulong b = 1;
+            for (ulong i = 2; i <= n; i ++)
+            {
+                var c = b;
+                b += a;
+                a = c;
+            }
+            return a;
         }
 
         public IEnumerable<ulong> Data()
