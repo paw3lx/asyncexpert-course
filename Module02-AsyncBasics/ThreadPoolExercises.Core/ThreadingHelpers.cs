a4f9b52caa347a2da4ef8c40dd00c0ae21b8c0da
paw3lx <paw3lx@gmail.com>
2020-10-13 22:30:17 +0200

    Implemented ThreadingHelpers

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -12,8 +12,13 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            var thread = new Thread(() =>
+            {
+                Execute(action, repeats, token, errorAction);
+            });
+            thread.Start();
 
-
+            thread.Join();
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
@@ -22,9 +27,37 @@ namespace ThreadPoolExercises.Core
             //   HINT: you may use `AutoResetEvent` to wait until the queued work item finishes
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
+            
+            var autoEvent = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem((stateInfo) =>
+            {
+                Execute(action, repeats, token, errorAction);
 
+                (stateInfo as AutoResetEvent)?.Set();
+            }, autoEvent);
 
+            autoEvent.WaitOne();
+        }
 
+        private static void Execute(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
+        {
+            for (int i = 0; i < repeats; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    errorAction?.Invoke(null);
+                    break;
+                }
+                try
+                {
+                    action();
+                }
+                catch (Exception e)
+                {
+                    errorAction?.Invoke(e);
+                    break;
+                }
+            }
         }
     }
 }
